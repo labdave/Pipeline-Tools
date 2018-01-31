fc7f895d38b1893bee25dda1875aa15e8d0d7dca
Alex Waldrop <amw117@duke.edu>
2018-01-31 09:21:21 -0500

    Added better comments to SampleSummary.

--- VcfQC/SampleSummary.py
+++ VcfQC/SampleSummary.py
@@ -1,11 +1,19 @@
 class SampleSummary(object):
     # Class for holding summary data about a VCF file
     def __init__(self, max_depth=500, max_qual=500, max_indel_len=250, num_afs_bins=20):
+
+        # Main summary where values are incremented
         self.summary        = {}
+
+        # Upper bin for depth, qual, indel length counts
         self.max_depth      = max_depth
         self.max_qual       = max_qual
         self.max_indel_len  = max_indel_len
+
+        # Number of bins to use for allele frequency spectrum counts
         self.num_afs_bins   = num_afs_bins
+
+        # Initialize count arrays for holding histogram counts for each data type
         self.depths         = [0] * (max_depth + 1)
         self.quals          = [0] * (max_qual + 1)
         self.inserts        = [0] * (max_indel_len + 1)
@@ -13,42 +21,43 @@ class SampleSummary(object):
         self.afs            = [0] * num_afs_bins
 
     def add_count(self, key):
-        # Increment a count by one
+        # Increment a count in the main summary by one
         if key in self.summary:
             self.summary[key] += 1
         else:
             self.summary[key] = 1
 
     def add_depth(self, depth):
-        # Add variant depth to depth counts
+        # Increment depth histogram bin corresponding to observed depth
         if depth >= self.max_depth:
             self.depths[self.max_depth] += 1
         else:
             self.depths[depth] += 1
 
     def add_qual(self, qual):
-        # Add variant quality to qual counts
+        # Increment variant histogram bin corresponding to observed variant quality score
         if qual >= self.max_qual:
             self.quals[self.max_qual] += 1
         else:
             self.quals[qual] += 1
 
     def add_del(self, del_len):
-        # Add deletion length to deletion length counts
+        # Increment deletion length histogram bin corresponding to observed deletion
         if del_len >= self.max_indel_len:
             self.deletes[self.max_indel_len] += 1
         else:
             self.deletes[del_len] += 1
 
     def add_ins(self, ins_len):
-        # Add insertion length to insertion length counts
+        # Increment insertion length histogram bin corresponding to observed insertion
         if ins_len >= self.max_indel_len:
             self.inserts[self.max_indel_len] += 1
         else:
             self.inserts[ins_len] += 1
 
     def add_aaf(self, aaf):
-        # Add alternative allele frequency (AAF) to AAF counts bin
-        # Determine bin membership and increment appropriate AFS count
+        # Add alternative allele frequency (AAF) to AAF histogram
+        # Determine bin membership
         aaf_bin = int(aaf//self.num_afs_bins)
+        # Increment appropriate count
         self.afs[aaf_bin] += 1
