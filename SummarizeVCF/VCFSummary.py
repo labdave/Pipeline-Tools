5d8cec2d07c7b265565c26201c33f56a0b675a59
Alex Waldrop <amw117@duke.edu>
2018-02-02 16:57:23 -0500

    Implemented multiple features in VCFSummary.
    Implemented __str__ method to print report.
    Can now include a set of required colnames that will be initialized regardless of whether they're found in the VCF.

--- SummarizeVCF/VCFSummary.py
+++ SummarizeVCF/VCFSummary.py
@@ -1,13 +1,13 @@
 from SummarizeVCF.SampleSummary import SampleSummary
 
 class VCFSummary(object):
-    def __init__(self, sample_names, count_names, **kwargs):
+    def __init__(self, sample_names, required_count_names=None, **kwargs):
 
         # List of samples to be contained in report
         self.sample_names   = sample_names
 
         # List of colnames that will be counted in report
-        self.count_names    = count_names
+        self.count_names    = [] if required_count_names is None else required_count_names
 
         # Upper bin for depth, qual, indel distribution summaries
         self.max_indel_len  = kwargs.get("max_indel_len",   100)
@@ -29,8 +29,16 @@ class VCFSummary(object):
                 summary[sample_name].init_count(count_name)
         return summary
 
-    def add_count(self, sample_name, key):
-        self.summary[sample_name].add_count(key)
+    def init_count_field(self, count_name):
+        # Initialize a new count field for all samples
+        for sample_name in self.sample_names:
+            self.summary[sample_name].init_count(count_name)
+        self.count_names.append(count_name)
+
+    def add_count(self, sample_name, count_name):
+        if count_name not in self.count_names:
+            self.init_count_field(count_name)
+        self.summary[sample_name].add_count(count_name)
 
     def add_depth(self, sample_name, depth):
         self.summary[sample_name].add_depth(depth)
@@ -49,4 +57,72 @@ class VCFSummary(object):
 
     def __str__(self):
         # Get string representation of current report
-        pass
+        return self.__get_count_data_string() + self.__get_depth_summary_string() + self.__get_qual_summary_string() + self.__get_afs_summary_string() + self.__get_insert_summary_string() + self.__get_delete_summary_string()
+
+    def __get_count_data_string(self):
+        to_ret = "#COUNTS\n"
+        to_ret += "\t".join(["Sample"] + self.count_names) + "\n"
+        for sample in self.sample_names:
+            sample_data = [sample] + [str(self.summary[sample].get_count(count_name)) for count_name in self.count_names]
+            to_ret += "\t".join(sample_data) + "\n"
+        return to_ret
+
+    def __get_depth_summary_string(self):
+        to_ret = "#DEPTH\n"
+        first_sample = True
+        for sample in self.sample_names:
+            labels, data = self.summary[sample].get_depth_summary()
+            if first_sample:
+                to_ret += "\t".join(["Sample"] + labels) + "\n"
+                first_sample = False
+            sample_data = [sample] + [str(x) for x in data]
+            to_ret += "\t".join(sample_data) + "\n"
+        return to_ret
+
+    def __get_qual_summary_string(self):
+        to_ret = "#QUAL\n"
+        first_sample = True
+        for sample in self.sample_names:
+            labels, data = self.summary[sample].get_qual_summary()
+            if first_sample:
+                to_ret += "\t".join(["Sample"] + labels) + "\n"
+                first_sample = False
+            sample_data = [sample] + [str(x) for x in data]
+            to_ret += "\t".join(sample_data) + "\n"
+        return to_ret
+
+    def __get_insert_summary_string(self):
+        to_ret = "#INSERT_LEN\n"
+        first_sample = True
+        for sample in self.sample_names:
+            labels, data = self.summary[sample].get_ins_summary()
+            if first_sample:
+                to_ret += "\t".join(["Sample"] + labels) + "\n"
+                first_sample = False
+            sample_data = [sample] + [str(x) for x in data]
+            to_ret += "\t".join(sample_data) + "\n"
+        return to_ret
+
+    def __get_delete_summary_string(self):
+        to_ret = "#DELETE_LEN\n"
+        first_sample = True
+        for sample in self.sample_names:
+            labels, data = self.summary[sample].get_del_summary()
+            if first_sample:
+                to_ret += "\t".join(["Sample"] + labels) + "\n"
+                first_sample = False
+            sample_data = [sample] + [str(x) for x in data]
+            to_ret += "\t".join(sample_data) + "\n"
+        return to_ret
+
+    def __get_afs_summary_string(self):
+        to_ret = "#AAFS\n"
+        first_sample = True
+        for sample in self.sample_names:
+            labels, data = self.summary[sample].get_aaf_summary()
+            if first_sample:
+                to_ret += "\t".join(["Sample"] + labels) + "\n"
+                first_sample = False
+            sample_data = [sample] + [str(x) for x in data]
+            to_ret += "\t".join(sample_data) + "\n"
+        return to_ret
