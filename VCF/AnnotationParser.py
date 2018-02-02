ce95520db1dc542286fac2d21a708d719b83c3ab
Alex Waldrop <amw117@duke.edu>
2018-02-02 16:56:11 -0500

    Modified AnnotationParser.
    Fixed bug in unpack_snpeff_annotations function.
    Several cosmetic improvements to the code.

--- VCF/AnnotationParser.py
+++ VCF/AnnotationParser.py
@@ -8,11 +8,11 @@ class AnnotationParser(object):
 
     def __init__(self, vcf_parser):
 
+        # Determine annotation type
+        self.annotation_type = self.calc_annotation_type(vcf_parser)
+
         # Get list of info fields that should appear in each VCF record in the correct order
         self.info_fields = VCFHelper.get_info_field_names(vcf_parser)
-
-        # Determine annotation type
-        self.annotation_type = VCFAnnotationType.SNPEFF if "ANN" in self.info_fields else VCFAnnotationType.ANNOVAR
 
         if self.annotation_type is VCFAnnotationType.SNPEFF:
             self.snpeff_info_fields = VCFHelper.get_snpeff_ann_field_names(vcf_parser)
@@ -23,7 +23,7 @@ class AnnotationParser(object):
         # Guaranteed to be in same order as they appear in VCF header
         if self.annotation_type is VCFAnnotationType.SNPEFF:
             # Unpack the snpeff 'ANN' annotation field into the record info
-            self.unpack_snpeff_info(record)
+            self.__unpack_snpeff_info(record)
 
         # Put data in correct order
         info_data = OrderedDict()
@@ -36,11 +36,28 @@ class AnnotationParser(object):
                 info_data[info_field] = info_data[info_field][0]
         return info_data
 
-    def unpack_snpeff_info(self, record):
+    def get_annotation_type(self):
+        # Return the annotation type
+        return self.annotation_type
+
+    def get_available_info_fields(self):
+        return self.info_fields
+
+    def __unpack_snpeff_info(self, record):
         # Unpack the snpeff 'ANN' annotation field into the record info
-        ann_info = record.INFO.pop("ANN")
+        ann_info = record.INFO.pop("ANN")[0].split("|")
         # Convert empty string to NoneType
         ann_info = [None if x == "" else x for x in ann_info]
         # Add new annotations to dictionary
         for i in range(len(self.snpeff_info_fields)):
             record.INFO[self.snpeff_info_fields[i]] = ann_info[i]
+
+    @staticmethod
+    def calc_annotation_type(vcf_parser):
+        # Determine the annotation format on a VCF file based on the info columns declared in the header
+        if "ANN" in vcf_parser.infos:
+            return VCFAnnotationType.SNPEFF
+        elif "ANNOVAR_DATE" in vcf_parser.infos:
+            return VCFAnnotationType.ANNOVAR
+        else:
+            return VCFAnnotationType.UNKNOWN
